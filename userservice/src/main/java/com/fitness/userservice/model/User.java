bdccce2eb80b94f81a2b133f80d3aad327c8886a
Yakup Balci <yakup.balci599@gmail.com>
2026-02-06 11:32:34 +0100

    Add user model

---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,33 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+  @Id
+  @GeneratedValue
+  private String id;
+  @Column(nullable = false, unique = true)
+  private String email;
+  @Column(nullable = false)
+  private String password;
+  private String firstName;
+  private String lastName;
+
+  @Enumerated(EnumType.STRING)
+  private UserRole role = UserRole.USER;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+}
